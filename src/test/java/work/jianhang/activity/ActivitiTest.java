155c8cfec0737b5426494dac854e42a98f6fa8e4
Grant Lin <grant.lin@42-q.com>
2019-08-30 13:02:02 +0800

    部署一个Activiti流程

---
+++ src/test/java/work/jianhang/activity/ActivitiTest.java
@@ -0,0 +1,30 @@
+package work.jianhang.activity;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.junit.Test;
+
+public class ActivitiTest {
+    /**
+     * 1、部署流程
+     * 2、启动流程实例
+     * 3、请假人发出请假申请
+     * 4、班主任查看任务
+     * 5、班主任审批
+     * 6、最终的教务处Boss审批
+     */
+
+
+    /**
+     * 1：部署一个Activiti流程
+     * 运行成功后，查看之前的数据库表，就会发现多了很多内容
+     */
+    @Test
+    public void createActivityTask() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        processEngine.getRepositoryService().createDeployment()
+                .addClasspathResource("shenqing.bpmn")
+                .addClasspathResource("shenqing.png")
+                .deploy();
+    }
+}
