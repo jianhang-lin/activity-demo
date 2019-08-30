79643caa25c343251e9cf4317737e444ae6201da
Grant Lin <grant.lin@42-q.com>
2019-08-30 12:56:48 +0800

    创建Activiti流的相关的数据库表

--- src/test/java/work/jianhang/activity/ActivitiTable.java
+++ src/test/java/work/jianhang/activity/ActivitiTable.java
@@ -1,4 +1,18 @@
 package work.jianhang.activity;
 
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.junit.Test;
+
 public class ActivitiTable {
+    /**
+     * 创建Activiti流的相关的数据库表
+     */
+    @Test
+    public void createTable() {
+        ProcessEngine processEngine = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml")
+                .buildProcessEngine();
+    }
+
+
 }
