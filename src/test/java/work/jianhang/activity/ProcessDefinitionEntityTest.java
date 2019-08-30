b7280fca388cf6461e91791cd5703f215970fc26
Grant Lin <grant.lin@42-q.com>
2019-08-30 16:02:06 +0800

    根据pdid得到processDefinitionEntity中的activityimpl

--- src/test/java/work/jianhang/activity/ProcessDefinitionEntityTest.java
+++ src/test/java/work/jianhang/activity/ProcessDefinitionEntityTest.java
@@ -24,4 +24,28 @@ public class ProcessDefinitionEntityTest {
         ProcessDefinitionEntity processDefinitionEntity = (ProcessDefinitionEntity) processEngine.getRepositoryService().getProcessDefinition("shenqing:3:15004");
         System.out.println(processDefinitionEntity.getId() + ":"+ processDefinitionEntity.getName());
     }
+
+    /**
+     * 根据pdid得到processDefinitionEntity中的activityimpl
+     */
+    @Test
+    public void testGetActivityImpl() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        // 根据pdid得到ProcessDefinitionEntry
+        ProcessDefinitionEntity processDefinitionEntity = (ProcessDefinitionEntity) processEngine.getRepositoryService().getProcessDefinition("shenqing:3:15004");
+        System.out.println(processDefinitionEntity.getId() + ":"+ processDefinitionEntity.getName());
+        /**
+         * ActivityImpl是一个对象
+         * 一个activityImpl代表processDefinitionEntity中的一个节点
+         */
+        List<ActivityImpl> activities = processDefinitionEntity.getActivities();
+        for (ActivityImpl activity : activities) {
+            System.out.println(activity.getId());
+            System.out.println("hegiht:" + activity.getHeight());
+            System.out.println("width:" + activity.getWidth());
+            System.out.println("x:" + activity.getX());
+            System.out.println("y:"+ activity.getY());
+        }
+    }
+
 }
