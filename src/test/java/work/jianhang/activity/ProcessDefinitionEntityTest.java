35883d8945e739faa4c903478e0918b903356c1a
Grant Lin <grant.lin@42-q.com>
2019-08-30 16:04:24 +0800

    得到ProcessDefinitionEntity中的所有的ActivityImpl的所有的PvmTransition

--- src/test/java/work/jianhang/activity/ProcessDefinitionEntityTest.java
+++ src/test/java/work/jianhang/activity/ProcessDefinitionEntityTest.java
@@ -48,4 +48,29 @@ public class ProcessDefinitionEntityTest {
         }
     }
 
+    /**
+     * 得到ProcessDefinitionEntity中的所有的ActivityImpl的所有的PvmTransition
+     */
+    @Test
+    public void testSequenceFlow() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        // 根据pdid得到ProcessDefinitionEntry
+        ProcessDefinitionEntity processDefinitionEntity = (ProcessDefinitionEntity) processEngine.getRepositoryService().getProcessDefinition("shenqing:3:15004");
+        System.out.println(processDefinitionEntity.getId() + ":"+ processDefinitionEntity.getName());
+        /**
+         * ActivityImpl是一个对象
+         * 一个activityImpl代表processDefinitionEntity中的一个节点
+         */
+        List<ActivityImpl> activities = processDefinitionEntity.getActivities();
+        for (ActivityImpl activity : activities) {
+            /**
+             * 得到一个activityimpl的所有的outgoing
+             */
+            List<PvmTransition> pvmTransitions = activity.getOutgoingTransitions();
+            for (PvmTransition pvmTransition : pvmTransitions) {
+                System.out.println("sequenceFlowId:"+pvmTransition.getId());
+            }
+        }
+    }
+
 }
