8dd8f279a63cd55b85a90fb9de7c56b7f49b1f33
Grant Lin <grant.lin@42-q.com>
2019-09-02 09:55:01 +0800

    更新请假流程：情况三的逻辑实现

--- src/test/java/work/jianhang/activity/TaskTest.java
+++ src/test/java/work/jianhang/activity/TaskTest.java
@@ -12,6 +12,8 @@ public class TaskTest {
 
 //    情况一：当没有进入该节点之前，就可以确定任务的执行人
 //  实例：比如进行“请假申请”的流程时候，最开始执行的就是提交”请假申请“，那么就需要知道，谁提交的“请假”，很明显，在一个系统中，谁登陆到系统里面，谁就有提交“请假任务”的提交人，那么执行人就可以确定就是登录人。那么执行人就可以确定就是登录人
+//    总结：针对情况一，那么我们必须要进入该节点执行前，就要分配一个执行人。
+
     @Test
     public void startDeployTestTask1() {
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
@@ -77,6 +79,41 @@ public class TaskTest {
 //    情况三：一个节点任务，之前是不存在执行人（未知），只有当符合身份的人，登陆系统，进入该系统，才能确定执行人。
 //    实例：比如，如果当前的流程实例正在执行“自荐信审批”，这个时候，自荐信审批没有任务执行人，因为审批人是可以很多个，无法确定到底是谁，只有当咨询员登录系统以后才能给该任务赋值执行人，即存在只要是咨询员登陆，那么就可以看到所有的“自荐信”。
 
+    @Test
+    public void startDeployTestTask3() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        processEngine.getRepositoryService()
+                .createDeployment()
+                .name("请假流程：情况三")
+                .addClasspathResource("task3.bpmn")
+                .addClasspathResource("task3.png")
+                .deploy();
+    }
+
+    @Test
+    public void testStartTask3() {
+        /**
+         * 流程变量
+         *   给<userTask id="请假申请" name="请假申请" activiti:assignee="#{student}"></userTask>
+         *     的student赋值
+         */
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("student", "小明");
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        processEngine.getRuntimeService().startProcessInstanceById("Task3:1:12504", variables);
+    }
+
+    /**
+     * 在完成请假申请的任务的时候，给班主任审批的节点赋值MyTaskListener定义的任务的执行人
+     */
+    @Test
+    public void testFinishTask3_Teacher(){
+        //Map<String, Object> variables = new HashMap<String, Object>();
+        //variables.put("teacher", "我是小明的班主任");
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        // processEngine.getTaskService().complete("2505", variables); //完成任务的同时设置流程变量
+        processEngine.getTaskService().complete("15005"); //完成任务
+    }
 //    情况四：一个任务节点有n多人能够执行该任务，但是只要有一个人执行完毕就完成该任务了：组任务
 //    实例：比如，“进入地铁站通道”的流程，我们一般地铁都是有N个安全检查的入口，有很多个人在进行检查，那么我们要想通过检查，那么任意一个检察员只要通过即可。
 }
